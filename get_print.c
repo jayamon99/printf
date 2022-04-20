2ae81c568d0f7120a14b5885da8bcf0c0af783d7
jayamon99 <henryquaye99@outlook.com>
2022-04-20 23:46:46 +0000

    fixed error

--- get_print.c
+++ get_print.c
@@ -6,7 +6,7 @@
  * @s: character that holds the conversion specifier
  * Description: the function loops through the structs array
  * func_arr[] to find a match between the specifier passed to _printf
- * and the first element of the struct, and then the approriate
+ * and the first element of the struct, and then the appropiate
  * printing function
  * Return: a pointer to the matching printing function
  */
